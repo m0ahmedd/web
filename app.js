0c0be6db364b69f6e93e7bcab1632db8b85a78c1
m0ahmedd <m0ahmedd722@gmail.com>
2023-05-04 21:50:54 +0300

    added: deleting tasks from custom To Do lists + lodash

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const _ = require("lodash");
 
 const app = express();
 
@@ -29,8 +30,18 @@ const taskSchema = new mongoose.Schema({
     }
 });
 
+const Task = mongoose.model("Task", taskSchema);
 
-const Task = mongoose.model("Tasks", taskSchema);
+
+const listSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter the list name!"]
+    },
+    tasks: [taskSchema]
+});
+
+const List = mongoose.model("List", listSchema);
 // const tasks = [];
 // const workTasks = [];
 
@@ -51,11 +62,12 @@ const multipleTasks = [{
 
 
 
-const today = new Date();
-const currentDay = today.getDay();
+const today_ = new Date();
+const currentDay = today_.getDay();
 let dayKind_ = "";
 const day = date.getDayName();
-
+const dayArray = day.split(",");
+const today = dayArray[0]+ ",";
 
 app.get("/", function (req, res) {
 
@@ -68,7 +80,7 @@ app.get("/", function (req, res) {
     Task.find()
         .then(function (tasks) {
             if (tasks.length === 0) {
-                
+
                 // If it was declared out of this function, when deleted mongoose won't be able to track it to save it; therefore, I declared it here!
                 const task1 = new Task({
                     name: "task1: Web Dev course"
@@ -104,36 +116,118 @@ app.post("/", function (req, res) {
     //     res.redirect("/");
     // }
     const newTaskName = req.body.newTask;
+    const listName = req.body.list;
+
     const task = new Task({
         name: newTaskName
     });
-    task.save();
-    res.redirect("/");
+
+    if(listName === today){
+        task.save();
+        res.redirect("/");
+    }
+    else{
+        List.findOne({
+            name: listName
+        })
+        .then(function (foundList) {
+            foundList.tasks.push(task);
+            foundList.save();
+            res.redirect("/"+ listName);
+        })
+        .catch((error)=> console.error("Failed to find list!"));
+    }
 });
+
 
 app.post("/delete", function (req, res) {
     const taskID = req.body.checkTask;
-    Task.findByIdAndDelete(taskID)
-        .then(function (docs) {
-            console.log("Task " + taskID + " is deleted!");
-            res.redirect("/");
+    const listName = req.body.listName;
+
+    if(listName === today){
+        Task.findByIdAndDelete(taskID)
+            .then(function (docs) {
+                console.log("Task " + taskID + " is deleted!");
+                res.redirect("/");
+                })
+            .catch((error) => console.error("Error in deleting task of id " + taskID + " !!"));
+            }
+    else{
+        List.findOneAndUpdate({name: listName},
+            {$pull: {tasks: {_id: taskID}}})
+            .then( function(){
+                console.log("Task " + taskID + " is deleted!");
+                res.redirect("/"+ listName);
+            });
+    }
+});
+
+
+
+app.get("/:listName", function (req, res) {
+    const listName = _.capitalize(req.params.listName);
+
+    List.findOne({
+            name: listName
         })
-        .catch((error) => console.error("Error in deleting task of id " + taskID + " !!"));
+        .then(function (foundList) {
+            if(foundList){
+                //Show the existing list
+                res.render("list", {
+                    dayKind: dayKind_,
+                    listTitle: foundList.name,
+                    addedTasks: foundList.tasks
+                });
+            }
+            else {
+                    console.log(listName + " is not found!!");
+                    // Create the list:
+                    const list = new List({
+                        name: listName,
+                        tasks: multipleTasks
+                    });
+                    list.save()
+                        .then(function () {
+                            console.log("list " + listName + " is created !");
+                           
+                            res.redirect("/" + listName);
+
+                        });
+                    // .catch((error)=> console.error("Error in creating "+ listName +" list!! => "+ error));
+                    // .then(()=> res.redirect("/"+ listName));
+                    // res.redirect("/"+ listName);
+            };
+        });
+
+    // const list = new List({
+    //     name: listName,
+    //     tasks: tasks
+    // });
+    // list.save()
+    // .then(function(){
+    //     console.log("list "+listName+ " is created !");
+    //     res.render("list", {
+    //         dayKind: dayKind_,
+    //         listTitle: listName.toUpperCase(),
+    //         addedTasks: tasks
+    //     });
+    //     })
+    //     .catch((err)=> console.error("Error in creating list "+listName+" => "+ err));
 });
 
 
-app.get("/work", function (req, res) {
-    res.render("list", {
-        dayKind: "work day",
-        listTitle: "Work List",
-        addedTasks: workTasks
-    });
-});
+// app.get("/work", function (req, res) {
+//     res.render("list", {
+//         dayKind: "work day",
+//         listTitle: "Work List",
+//         addedTasks: workTasks
+//     });
+// });
 
 
-app.get("/about", function (req, res) {
-    res.render("about");
-});
+// app.get("/about", function (req, res) {
+//     res.render("about");
+// });
 
 app.listen(port, function () {
     console.log("Server started on port " + port);
